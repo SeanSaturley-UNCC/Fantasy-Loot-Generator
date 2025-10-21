0da7d1cbeb2815379e9b64289b59eb9ad4d8ccdf
Peyton Santo <peyton.santo429@gmail.com>
2025-10-21 12:57:37 -0400

    fixing merge conflicts

--- models/User.js
+++ models/User.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const { hashPassword } = require('../utils');
 
-
 // schema definning
 const userSchema = new mongoose.Schema(
   {
